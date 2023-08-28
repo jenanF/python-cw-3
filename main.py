1ca8cf6d6795988ea843f683cdf27b6bad66357c
jenanF <108689302+jenanF@users.noreply.github.com>
2023-08-28 19:28:31 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # write your code here
+favorite_animals = ["cat", "fish", "dog", "monkey"]
+print(favorite_animals)
+print(favorite_animals[1])
+favorite_animals.pop(2)
+print(favorite_animals)
+
+# PART 2
+favorite_animals.append("rabbit")
+for anim in favorite_animals:
+    print("i love", anim)
+
+# PART 3
+numbers = [1, 2, 3, 4, 5]
+numbers_sum = 0
+
+for num in numbers:
+  numbers_sum += num
+  print(numbers_sum)
